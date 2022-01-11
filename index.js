0b1b066ac583c183af7497477c79f9c228ee0311
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-11 10:21:50 -0500

    commit tree 7b64c721622d837b3eb99d45df9e8113e2221395

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide () {
+    return 2000 / 100;
+}
+console.log(divide());
+
+const square = num => num * num;
+console.log(square(2));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(2,3));
